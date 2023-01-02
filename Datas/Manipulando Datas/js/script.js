334e0c7b98e58d87abd545db5671a3ba81831c49
thiagocef <thicef@gmail.com>
2023-01-02 11:16:12 -0300

    datas em formato local

--- Datas/Manipulando Datas/js/script.js
+++ Datas/Manipulando Datas/js/script.js
@@ -1,10 +1,10 @@
-const hoje = new Date() //data atual
+const hoje = new Date() //data atual completa
 console.log("Hoje: ", hoje)
 
 let dia = hoje.getDate() //retorna a data do mês
 console.log("Dia: ", dia)
 
-let mes = hoje.getMonth() //retorna o mês em número
+let mes = hoje.getMonth() + 1 //retorna o mês em número
 console.log("Mês: ", mes)
 
 let ano = hoje.getFullYear() //retorna o ano
@@ -18,4 +18,18 @@ let nextMonth = hoje.getMonth() //mês atual
 hoje.setMonth(nextMonth + 1) //altera o mês, adicionando 1 ao mês atual
 console.log("Próximo mês: ", hoje)
 
+let hora = hoje.getHours()
+let minutos = hoje.getMinutes()
+console.log(`Hora: ${hora}:${minutos}`)
 
+hoje.setHours(hora + 1)
+console.log("Hora mais 1: ", hoje + 1) //soma um à hora atual
+
+//----------------------------------------------------
+//----------------------------------------------------
+
+const dataBr = new Date()
+console.log('------------------------------')
+console.log("Data no formato do idioma local: ", dataBr.toLocaleDateString())
+
+console.log(dataBr.getMonth() + 1)
